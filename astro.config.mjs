aff964764aebe254a959a033177dab01b82a0cb7
Simar710 <69190250+Simar710@users.noreply.github.com>
2023-09-06 20:23:26 -0400

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://Simar710.github.io',
+  base: '/Personal-Portfolio'
+});
