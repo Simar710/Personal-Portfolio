07655d1656c3b85c82b544ed8ba54fcf4dd1c6f4
Simar710 <simardeepsingh710@gmail.com>
2024-11-16 21:18:28 -0500

    push node files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Simar710.github.io',
-  base: '/Personal-Portfolio'
+  base: '/Personal-Portfolio',
+  integrations: [react()],
 });
