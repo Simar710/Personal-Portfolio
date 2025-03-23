4b9ec619a0cb713b45635adcdef90d703313118e
Simardeep Singh <simardeepsingh710@gmail.com>
2025-03-23 16:17:56 -0400

    Updated the Portfolio (#9)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Simar710.github.io',
-  base: '/Personal-Portfolio',
+  base: '/Personal-Portfolio', // Ensure this is correctly set
   integrations: [react()],
 });
